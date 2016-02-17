41cdf267f81a202a3ca0f4400a9a92a70ffa685b
Kenny Tran <kenny8tran@gmail.com>
2016-02-16 20:09:50 -0800

    Create ability to read special config file

--- special.js
+++ special.js
@@ -8,32 +8,71 @@ var purify = require('purify-css');
 
 var sys = require('sys')
 var exec = require('child_process').exec;
-function puts(error, stdout, stderr) { sys.puts(stdout) }
 
 var args = argv._;
 
-function purifyFile() {
-  var filePaths = glob.sync('node_modules/' + args[1] + '/**/*.css')
+function puts(error, stdout, stderr) {
+  sys.puts(stdout);
+}
+
+function purifyFile(packageName, options) {
+  var filePaths = glob.sync('**/node_modules/' + packageName + '/**/*.css')
     .filter(function (filePath) {
       // Ignore minified files.
       return filePath.indexOf('.min') === -1;
     });
-  var selectedSelectors = args.slice(2);
-  var purifyOptions = {
-    output: './purified.css',
-    whitelist: selectedSelectors
-  };
 
-  purify('', filePaths, purifyOptions);
-  console.log('Installed:', selectedSelectors);
+  console.log('Installed:', options.whitelist, 'from', packageName);
+  return purify('', filePaths, options);
+}
+
+function installPackage(packageName, cb) {
+  exec('npm install ' + packageName, cb);
 }
 
 if (args[0] === 'install') {
-  exec('npm install ' + args[1], function (error, stdout, stderr) {
-    puts(error, stdout, stderr);
 
-    if (!error) {
-      purifyFile();
+  if (args[1]) {
+    var packageName = args[1];
+    var selectedSelectors = args.slice(2);
+    installPackage(packageName, function (error, stdout, stderr) {
+      puts(error, stdout, stderr);
+
+      if (!error) {
+        purifyFile(packageName, {
+          output: './special.css',
+          whitelist: selectedSelectors
+        });
+      }
+    });
+  } else {
+    if (fs.existsSync('./special.config.js')) {
+      var config = require('./special.config.js');
+      var packages = config.packages;
+      var purified = '';
+      var finishCount = 0;
+      var packagesCount  = Object.keys(packages).length;
+
+      var tasks = Object.keys(packages).forEach(function (packageName) {
+        var purifyOptions = {
+          whitelist: packages[packageName].selectors
+        };
+
+        installPackage(packageName, function () {
+          purified = purified + '\n\n/* break */\n\n' + purifyFile(packageName, purifyOptions);
+          finishCount++;
+          if (finishCount === packagesCount) {
+            fs.writeFile('./special.css', purified, function (err) {
+              if (err) {
+                return err;
+              }
+            });
+          }
+        });
+      });
+
     }
-  });
+
+  }
+
 }
